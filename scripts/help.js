df72f4fab258711eae21f26839fb30a2fedd72d5
jwucong <jwucong@gmail.com>
2023-07-14 01:58:42 +0800

    update

--- scripts/help.js
+++ scripts/help.js
@@ -1,29 +1,42 @@
-
-const tips = [
-  '[Tips]: The command format is as follows:\n',
-  'npm run pkg [command] [PackageName]\n',
-  'command:',
-  '  - add  add a new package.',
-  '  - remove  delete an existing package.',
-  ''
-].join('\n')
-
-const commandValidators = {
-  add: (command, pkg) => {
-    return command.toLowerCase() === 'add' && pkg.trim().length > 0
+const commands = {
+  add: {
+    desc: 'Add a new package.',
+    validator: (command, pkg) => {
+      return command.toLowerCase() === 'add' && pkg.trim().length > 0
+    }
   },
-  remove: (command, pkg) => {
-    return command.toLowerCase() === 'remove' && pkg.trim().length > 0
-  },
-  default: () => false
+  remove: {
+    desc: 'Delete an existing package.',
+    validator: (command, pkg) => {
+      return command.toLowerCase() === 'remove' && pkg.trim().length > 0
+    }
+  }
 }
 
+const genCommandDescs = map => {
+  const commands = Object.keys(map)
+  const maxSize = Math.max.apply(null, commands.map(item => item.length))
+  const gen = command => {
+    const space = new Array(maxSize + 2 - command.length).fill(' ').join('')
+    const { desc } = map[command]
+    return ['  - ', command, space, desc].join('')
+  }
+  return commands.map(gen)
+}
+
+const tips = [
+  'Usage: npm run pkg <command> <package_name>',
+  'command:',
+  ...genCommandDescs(commands),
+  'Example: npm run pkg add test-pkg'
+].join('\n')
+
 export const commandValidator = (command, ...args) => {
-  const validator = commandValidators[command]
+  const { validator } = commands[command] || {}
   if (typeof validator === 'function') {
     return validator(command, ...args)
   }
-  return commandValidators.default(command, ...args)
+  return false
 }
 
 export const help = () => {
